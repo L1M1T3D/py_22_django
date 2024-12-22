d59bc5b53ca45c4ed64ddd51e1c13033cedb395f
sapphir0v <6.sapphirov.6@bk.ru>
2024-12-23 02:18:41 +0300

    homework #24 done

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 def home(request):
     return render(request, 'catalog/home.html')
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'catalog/product_list.html', {'products': products})
